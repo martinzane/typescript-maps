3eac77daa0533b8a2d35680540ec30a8b4c6ca3a
Martin Zane <martin@zaneweb.com>
2024-04-23 19:20:40 +0200

    Define Company class

---
+++ src/company.ts
@@ -0,0 +1,21 @@
+import { faker } from "@faker-js/faker";
+
+class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+}
+
+export default Company;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import Company from "./company";
 import User from "./user";
 
 const user = new User();
 
 console.log(user);
+
+const company = new Company();
+
+console.log(company);
